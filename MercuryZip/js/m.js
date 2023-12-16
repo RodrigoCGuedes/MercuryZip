15d821b87eca0c3a23ccae195b6fd7a21bc810ed
Rodrigo <136197449+RodrigoCGuedes@users.noreply.github.com>
2023-12-16 09:24:03 -0300

    Atualização 0.04
    Alterações principalmente na pg m (mediador).
    
    --> Implementação em js;
    --> Alterações em html e css;

--- MercuryZip/js/m.js
+++ MercuryZip/js/m.js
@@ -8,3 +8,75 @@ document.addEventListener('DOMContentLoaded', function () {
     const imagemVerification = document.getElementById('verification-image');
     imagemVerification.src = `/MercuryZip/images/num/${obterImagemAleatoria()}`;
 });
+
+function descriptografarUrl(codigo) {
+    // Extrair o padrão inicial (A ou B)
+    const padraoInicial = codigo.charAt(0);
+
+    // Determinar o protocolo com base no padrão inicial
+    let protocolo;
+    if (padraoInicial === 'A') {
+        protocolo = 'http://';
+    } else if (padraoInicial === 'B') {
+        protocolo = 'https://';
+    } else {
+        // Padrão desconhecido, retornar erro ou tratar conforme necessário
+        return null;
+    }
+
+    // Remover o padrão inicial do código
+    const codigoRestante = codigo.slice(1);
+
+    // Decodificar os caracteres restantes
+    let urlOriginal = '';
+    for (let i = 0; i < codigoRestante.length; i++) {
+        const charCode = codigoRestante.charCodeAt(i);
+        const caractereOriginal = String.fromCharCode(charCode - 1);
+        urlOriginal += caractereOriginal;
+    }
+
+    // Reconstruir o URL original
+    return protocolo + 'www.' + urlOriginal;
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    const imagemVerification = document.getElementById('verification-image');
+    const textoInput = document.getElementById('texto-input');
+    const contadorTentativas = document.getElementById('tentativas-contador').querySelector('span');
+    let tentativasRestantes = 3;
+
+    function verificarCorrespondencia(event) {
+        if (event.key === 'Enter') {
+            const numeroImagem = imagemVerification.src.match(/\/(\d+)\.png$/)[1];
+            const textoDigitado = textoInput.value;
+
+            if (textoDigitado === numeroImagem) {
+                alert('Correto! O número corresponde à imagem.');
+                tentativasRestantes--;
+
+                if (tentativasRestantes === 0) {
+                    window.location.href = descriptografarCodigo(window.location.href);
+                } else {
+                    // Atualizar a imagem para a próxima tentativa
+                    imagemVerification.src = `/MercuryZip/images/num/${obterImagemAleatoria()}`;
+                }
+            } else {
+                alert('Incorreto! Tente novamente.');
+                tentativasRestantes--;
+
+                if (tentativasRestantes === 0) {
+                    alert('Você excedeu o número de tentativas.');
+                    location.reload();
+                }
+            }
+
+            // Atualizar o contador de tentativas
+            contadorTentativas.textContent = tentativasRestantes;
+
+            // Limpar o conteúdo da caixa de texto após a verificação
+            textoInput.value = '';
+        }
+    }
+
+    textoInput.addEventListener('keypress', verificarCorrespondencia);
+});
